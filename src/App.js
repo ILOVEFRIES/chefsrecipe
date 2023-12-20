205633cd96d09229a8333f647f1b194282c4f8c3
DonutDaEarth <ss1.kurniawan@gmail.com>
2023-12-20 21:16:52 +0700

    Feat: Landing Page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import Login from './components/auth/Login';
 import SignUp from './components/auth/SignUp';
 import AuthDetails from './components/auth/AuthDetails';
 import AppHeader from './components/AppHeader';
+import LandingPage from './components/screens/LandingPage';
 
 function App() {
   return (
     <>
       <AppHeader/>
+        <LandingPage/>
+
         {/* <Login />
         <SignUp />
         <AuthDetails /> */}
