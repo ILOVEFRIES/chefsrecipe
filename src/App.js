933b9cd74d379b26b555ad740814f43e4eb17b7c
ILOVEFRIES <sutardjawilbert@gmail.com>
2023-12-20 18:46:53 +0700

    feats(authentication): Added sign up and authentication details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Login from './components/auth/Login';
+import SignUp from './components/auth/SignUp';
+import AuthDetails from './components/auth/AuthDetails';
 
 function App() {
   return (
     <div className='App'>
       <Login />
+      <SignUp />
+      <AuthDetails />
     </div>
   );
 }
